a7e01e4312241da5b1b2e640bf20494b8bbeeba1
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-09-06 15:35:22 +0500

    Начал выполнять задание №4

---
+++ task_4.js
@@ -0,0 +1,37 @@
+// Задание 4.
+
+// Реализовать следующее консольное приложение на прототипах.
+// Определить иерархию электроприборов. Включить некоторые в розетку. Посчитать 
+// потребляемую мощность. 
+// Таких приборов должно быть, как минимум, два (например, настольная лампа и 
+// компьютер). Выбрав прибор, подумайте, какими свойствами он обладает.
+
+
+function Device(power) {
+    this.power = power,
+    this.isOn = false,
+    
+    this.turnOn = function() {
+        this.isOn = true;
+    },      
+    
+    this.turnOff = function() {
+        this.isOn = false;
+    },
+    
+    this.isTurnedOn = function() {
+        return this.isOn;
+    }
+}
+
+
+const lamp = new Device(50);
+const pc = new Device(500);
+
+console.log(lamp.isTurnedOn())
+console.log(pc.isTurnedOn())
+
+lamp.turnOn()
+
+console.log(lamp.isTurnedOn())
+console.log(pc.isTurnedOn())
