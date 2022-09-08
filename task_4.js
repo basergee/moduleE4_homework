218d44fe4f70c2248777993195822012350a6868
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-09-08 09:25:26 +0500

    Добавил функцию расчета потребляемой мощности, добавил комментарии и изменил порядок строк

--- task_4.js
+++ task_4.js
@@ -1,42 +1,68 @@
 // Задание 4.
 
 // Реализовать следующее консольное приложение на прототипах.
-// Определить иерархию электроприборов. Включить некоторые в розетку. Посчитать 
-// потребляемую мощность. 
-// Таких приборов должно быть, как минимум, два (например, настольная лампа и 
+// Определить иерархию электроприборов. Включить некоторые в розетку. Посчитать
+// потребляемую мощность.
+// Таких приборов должно быть, как минимум, два (например, настольная лампа и
 // компьютер). Выбрав прибор, подумайте, какими свойствами он обладает.
 
 
 function Device(power) {
     this.power = power,
     this.isOn = false,
-    
+
     this.turnOn = function() {
         this.isOn = true;
-    },      
-    
+    },
+
     this.turnOff = function() {
         this.isOn = false;
     },
-    
+
     this.isTurnedOn = function() {
         return this.isOn;
     }
 }
 
 
+// Считает потребляемую мощность включенных устройств из массива divices
+const calcPower = function(devices) {
+    let p = 0;
+    for (let d = 0; d < devices.length; d++) {
+        if (devices[d].isTurnedOn()) {
+            p += devices[d].power;
+        }
+    }
+
+    return p;
+}
+
+// определим объекты
 const lamp = new Device(50);
 lamp.colorTemp = "Warm"
 
 const pc = new Device(500);
 pc.memoryInGb = 8
 
+// сложим все объекты в массив
+const devices = [lamp, pc];
+
+// проверим состояние некоторых объектов
 console.log(lamp.isTurnedOn())
 console.log(pc.isTurnedOn())
+console.log(pc.memoryInGb)
 
+// включим лампу
 lamp.turnOn()
 
+// узнаем потребляемую мощность устройств
+console.log(calcPower(devices));
+
 console.log(lamp.isTurnedOn())
 console.log(pc.isTurnedOn())
 
-console.log(pc.memoryInGb)
+// включим компьютер
+pc.turnOn()
+
+// узнаем потребляемую мощность устройств
+console.log(calcPower(devices));
