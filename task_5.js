255365dc2bfb27f1c36669240d68a7a9be65b732
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-09-08 23:22:25 +0500

    Добавил классы

--- task_5.js
+++ task_5.js
@@ -2,21 +2,100 @@
 
 // Переписать консольное приложение из задания №4 на классы.
 
-class Device() {
+class Device {
     constructor(power) {
         this.power = power;
         this.isOn = false;
     }
 
-    this.turnOn = function() {
+    turnOn = function() {
         this.isOn = true;
-    },
+    }
 
-    this.turnOff = function() {
+    turnOff = function() {
         this.isOn = false;
-    },
+    }
 
-    this.isTurnedOn = function() {
+    isTurnedOn = function() {
         return this.isOn;
     }
 }
+
+
+// Считает потребляемую мощность включенных устройств из массива divices
+const calcPower = function(devices) {
+    let p = 0;
+    for (let d = 0; d < devices.length; d++) {
+        if (devices[d].isTurnedOn()) {
+            p += devices[d].power;
+        }
+    }
+
+    return `Потребляемая мощность ${p} кВт`;
+}
+
+
+// создадим классы-наследники: лампа и компьютер
+
+class Lamp extends Device {
+    constructor(power, colorTemp) {
+        super(power);
+        this.colorTemp = colorTemp;
+    }
+
+    getColorTemp = function() {
+        console.log('Свет лампы: ' + this.colorTemp);
+    }
+}
+
+
+class Computer extends Device {
+    constructor(power, memorySizeInGb) {
+        super(power);
+        this.memorySizeInGb = memorySizeInGb;
+    }
+
+    getMemSize = function() {
+        console.log('Объем памяти компьютера: ' + this.memorySizeInGb + 'Гб');
+    }
+}
+
+
+// определим объекты
+const lamp = new Lamp(50, "Warm");
+
+const pc = new Computer(500, 8);
+
+// сложим все объекты в массив
+const devices = [lamp, pc];
+
+console.log('Выведем свойства объектов');
+lamp.getColorTemp();
+pc.getMemSize();
+
+console.log('------------------------')
+
+console.log('Проверим состояние некоторых объектов')
+console.log('Лампа включена? ' + lamp.isTurnedOn())
+console.log('Компьютер включен? ' + pc.isTurnedOn())
+
+console.log('------------------------')
+
+console.log('Включим лампу')
+lamp.turnOn()
+
+// узнаем потребляемую мощность устройств
+console.log(calcPower(devices));
+
+console.log('Лампа включена? ' + lamp.isTurnedOn())
+console.log('Компьютер включен? ' + pc.isTurnedOn())
+
+console.log('------------------------')
+
+console.log('Включим компьютер')
+pc.turnOn()
+
+// узнаем потребляемую мощность устройств
+console.log('Лампа включена? ' + lamp.isTurnedOn())
+console.log('Компьютер включен? ' + pc.isTurnedOn())
+console.log(calcPower(devices));
